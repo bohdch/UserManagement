2c38c04b848c669b50a27e2d0e7cbeb7caecc9dd
bohdch <bohdanchumak64@gmail.com>
2023-07-05 16:59:50 +0300

    Add an admin page

---
+++ src/Controllers/AdminAuthController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagement.Controllers
+{
+	public class AdminAuthController : Controller
+    {
+        [Route("/admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
